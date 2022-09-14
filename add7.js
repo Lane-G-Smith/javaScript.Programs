8753ab5ef31aa92cc6a6783c9b9018bd3a69bfbb
Lane Smith <108886772+Lane-G-Smith@users.noreply.github.com>
2022-09-14 13:49:23 -0700

    Update add7.js

--- add7.js
+++ add7.js
@@ -1,7 +1,7 @@
-let startWord = prompt("give me a number and I will add seven to it");
+let startWord = prompt("Give me a number and I will add seven to it.");
 let startNumber = parseInt(startWord);
 function add7() { 
   let endNumber = (startNumber + 7);
-return endNumber;
+  return endNumber;
 }
 alert(add7(startNumber));
