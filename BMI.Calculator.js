df502bb688c8c117fb71595e0962bfc4e1fe5536
Lane Smith <108886772+Lane-G-Smith@users.noreply.github.com>
2022-09-14 14:00:20 -0700

    Update BMI.Calculator.js

--- BMI.Calculator.js
+++ BMI.Calculator.js
@@ -1,12 +1,12 @@
-
+let weightString = prompt("What is your weight in pounds?");
+let heightString = prompt("What is your height in inches?");
+let weight = parseInt(weightString);
+let height = parseInt(heightString);
 
 function bmiCalculator(weight, height) {
     let bmi = (weight *703) / Math.pow(height, 2);
     return Math.round(bmi);
 }
-
-// place weight in pounds followed by height in inches where green numbers are
-
-let bmi = bmiCalculator(215, 73);
+let bmi = bmiCalculator(weight, height);
 bmi = bmi.toFixed(2);
-console.log (bmi + "......18.5-24.9 is normal, 25-29.9 is overweight, 30-34.9 is obese");
+alert(bmi + "......18.5-24.9 is normal, 25-29.9 is overweight, 30-34.9 is obese");
