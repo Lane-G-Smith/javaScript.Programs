100b7edf8c79007370191f1bab867f91263ffc72
Lane <highsierrasmith1@gmail.com>
2022-09-27 15:10:03 -0700

    Add HTML files for easy program deployment

--- 99BottlesOfBeer/99BottlesOfBeer.js
+++ 99BottlesOfBeer/99BottlesOfBeer.js
@@ -1,13 +1,19 @@
 let bottles = 99;
+let counter = 0;
 
 function onTheWall() {
-    
       if (bottles >= 1)
-      { console.log(bottles + " bottles of beer on the wall,"  + bottles  + " bottles of beer, take one down and pass it around, " + bottles + " bottles of beer on the wall ");
-       bottles - 1;
-    } else if (bottles < 1)  
-      { console.log("no more bottles of beer on the wall, no more bottles of beer, take one down and pass it around, no more bottles of beer on the wall");
-       bottles + 99;
-    }    
-    
+      { (counter ++);
+         console.log(bottles + " bottles of beer on the wall,"  + bottles  + " bottles of beer, take one down and pass it around, " + bottles + " bottles of beer on the wall ");
+       return (bottles = (bottles -= 1));
+    } else if (bottles < 1)
+      { (counter ++);
+         console.log("no more bottles of beer on the wall, no more bottles of beer, take one down and pass it around, no more bottles of beer on the wall");
+       return (bottles = (bottles + 99));
+    }        
 }
+
+if (counter <= 200) {
+   onTheWall(bottles, counter);
+}
+else console.log("The end");
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # javaScript.Programs
-Simple programs using javaScript from various sources of educational materials as well as a few that I made up along the way.
+Simple programs using javaScript from various sources of educational materials as well as a few that I made up or modified along the way.
