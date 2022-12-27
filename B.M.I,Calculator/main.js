55113709822914e7318b566350d1b5ab19284f41
Lane <highsierrasmith1@gmail.com>
2022-12-27 10:02:07 -0700

    update

---
+++ B.M.I,Calculator/main.js
@@ -0,0 +1,12 @@
+let weightString = prompt("What is your weight in pounds?");
+let heightString = prompt("What is your height in inches?");
+let weight = parseInt(weightString);
+let height = parseInt(heightString);
+
+function bmiCalculator() {
+    let bmi = (weight *703) / Math.pow(height, 2);
+    return Math.round(bmi);
+}
+let bmi = bmiCalculator(weight, height);
+bmi = bmi.toFixed(2);
+alert("your BMI is " + bmi + "...... 18.5-24.9 is normal, 25-29.9 is overweight, 30-34.9 is obese");
--- daysleft/main.js
+++ daysleft/main.js
@@ -10,6 +10,7 @@ if (today.getMonth() == 05 && today.getDate()>30)
     endDay.setFullYear(endDay.getFullYear()+1); 
 }
 // 1000ms/minute/hour/day
+
 let oneDay = 1000*60*60*24;
 
 let oneWeek = (oneDay*7);
@@ -21,10 +22,14 @@ let totalDays = ((Math.ceil((endDay.getTime()-today.getTime())/(oneDay)))*-1);
 let totalWorkDays = (((Math.ceil((endDay.getTime()-today.getTime())/(oneDay)/7*5))*-1));
 let totalWeeks = ((Math.ceil((endDay.getTime()-today.getTime())/(oneWeek))*-1));
 let totalMonths = ((Math.ceil((endDay.getTime()-today.getTime())/(oneMonth))*-1));
+let totalWorkHours = totalWorkDays*8;
 
 // css class select and replace text with function output
 document.querySelector([".monthsToProgram"]).textContent = totalMonths;
 document.querySelector([".weeksToProgram"]).textContent = totalWeeks;
 document.querySelector([".daysToProgram"]).textContent = totalDays;
 document.querySelector([".workDaysToProgram"]).textContent = totalWorkDays;
+document.querySelector([".workHours"]).textContent = totalWorkHours;
+
+
     
